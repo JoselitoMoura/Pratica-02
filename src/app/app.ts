2f442fd7a1095715356cdcfa387dec1f0820793e
JoselitoMoura <joselitodemourabarbosa>
2025-11-21 12:53:11 +0000

    Realizadas algumas alterações na estrutura do rodapé:
    Alterados os títulos do inglês para português.
    Realizadas alterações na estrutura do corpo:
    Inseridos textos com explicação sobre os títulos.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./componentes/header/header";
+import { Main } from "./componentes/main/main";
+import { Footer } from "./componentes/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Main, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Pratica-02');
+  protected readonly title = signal('UC00604-Programa-WEB');
 }
